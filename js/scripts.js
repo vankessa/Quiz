61fd15c4638001b8e9c9fc887e627a996781b172
ujeneza vanessa <vankessa@gmail.com>
2018-12-20 17:54:29 +0200

    added  script

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
-// $(document).ready(function() {
-//     $('form').submit(function(event) {
-//         event.preventDefault();
+$(document).ready(function() {
+    var optionsRadios1 = $("input:radio[name=optionsRadios1]:checked").val();
+    var optionsRadios2 = $("input:radio[name=optionsRadios2]:checked").val();
+    var optionsRadios3 = $("input:radio[name=optionsRadios3]:checked").val();
+    var optionsRadios4 = $("input:radio[name=optionsRadios4]:checked").val();
+    var optionsRadios5 = $("input:radio[name=optionsRadios5]:checked").val();
+    
+//    $('form').submit(function(event) {
+//        event.preventDefault();
 
 //         var firstNumber = parseInt($('#first-number').val());
 //         var secondNumber = parseInt($('#second-number').val());
@@ -8,13 +14,6 @@
 //         var sum = add(firstNumber, secondNumber);
 
 //         $('#result').text(sum);
-//     })
-// });
+//    })
+});
 
-// // BUSINESS LOGIC
-
-// function add(firstNumber, secondNumber) {
-//     var result = firstNumber + secondNumber;
-
-//     return result;
-// }
