f6ca5881aef2100111cc15723a31b0cc902103e1
ujeneza vanessa <vankessa@gmail.com>
2018-12-20 12:38:54 +0200

    added check box for question1

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// $(document).ready(function() {
+//     $('form').submit(function(event) {
+//         event.preventDefault();
+
+//         var firstNumber = parseInt($('#first-number').val());
+//         var secondNumber = parseInt($('#second-number').val());
+
+//         var sum = add(firstNumber, secondNumber);
+
+//         $('#result').text(sum);
+//     })
+// });
+
+// // BUSINESS LOGIC
+
+// function add(firstNumber, secondNumber) {
+//     var result = firstNumber + secondNumber;
+
+//     return result;
+// }
