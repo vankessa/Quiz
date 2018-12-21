aa55189ebe672652592c9c74169b284d40ac847c
ujeneza vanessa <vankessa@gmail.com>
2018-12-21 08:21:14 +0200

    added content

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,19 @@ $(document).ready(function() {
     var optionsRadios3 = $("input:radio[name=optionsRadios3]:checked").val();
     var optionsRadios4 = $("input:radio[name=optionsRadios4]:checked").val();
     var optionsRadios5 = $("input:radio[name=optionsRadios5]:checked").val();
+    $("#radioButtons form").submit(function(event) {
+        var radioButtons = ['optionsRadios6','optionsRadios7','optionsRadios8','optionsRadios9','optionsRadios10','optionsRadios11',];
+  
+        radioButtons.forEach(function(radioButton) {
+          var userInput = $("input#" + radioButton).val();
+          $("." + radioButton).text(userInput);
+        });
+  
+        $("#marks").show();
+  
+        event.preventDefault();
+      });
+    });
     
 //    $('form').submit(function(event) {
 //        event.preventDefault();
@@ -15,5 +28,5 @@ $(document).ready(function() {
 
 //         $('#result').text(sum);
 //    })
-});
+
 
